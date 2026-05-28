2f59b6cf2003c458d373a0ce26f3743ae475a516
mani <cr4m21@gmail.com>
2026-05-28 17:53:57 +0530

    add file index and express pkg

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log(`hello world from github action maniram : ${process.pid}`);
+  res.send(`hello world from github action maniram : ${process.pid}`);
+});
+
+app.get("/crash", (req, res) => {
+  setTimeout(() => {
+    throw new Error(`app crash ho gya hai: ${process.pid}`);
+  }, 1000);
+});
+
+app.listen(3000, () => {
+  console.log(`server start at port 3000`);
+});
